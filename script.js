bf44561ed15b07b20944615f67098443e03d3636
Keerthi-Reddy-Baripireddy <144705049+Keerthi-Reddy-Baripireddy@users.noreply.github.com>
2023-11-16 20:49:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
 function daysOfAYear(year) {
-    if (year % 4 === 0) {
-       
-        if (year % 100 === 0) {
-            if (year % 400 === 0) {
-               
-                return 366;
-            } else {
-              
-                return 365;
-            }
-        } else {
-             
-            return 366;
-        }
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        return 366; // Leap year
     } else {
-        
-        return 365;
+        return 365; // Non-leap year
     }
 }
 
-
-console.log(daysOfAYear(2022)); 
-console.log(daysOfAYear(2024)); 
-console.log(daysOfAYear(1900)); 
-console.log(daysOfAYear(2000));
+// Example usage
+console.log(daysOfAYear(2022)); // Output: 365
+console.log(daysOfAYear(2024)); // Output: 366
+console.log(daysOfAYear(1900)); // Output: 365
+console.log(daysOfAYear(2000)); // Output: 366
