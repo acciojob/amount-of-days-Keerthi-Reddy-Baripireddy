f727b4dacf0f40460bbefa18c8bc41a88475cb10
Keerthi-Reddy-Baripireddy <144705049+Keerthi-Reddy-Baripireddy@users.noreply.github.com>
2023-11-24 11:44:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 function daysOfAYear(year) {
-    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
-        return 366; // Leap year
-    } else {
-        return 365; // Non-leap year
-    }
+  // Check if the year is a leap year
+  const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+
+  // Return the number of days based on leap year or not
+  return isLeapYear ? 366 : 365;
 }
 
-// Example usage
-console.log(daysOfAYear(2022)); // Output: 365
-console.log(daysOfAYear(2024)); // Output: 366
-console.log(daysOfAYear(1900)); // Output: 365
-console.log(daysOfAYear(2000)); // Output: 366
+// Get user input for the year
+var userInput = prompt("Enter a year:");
+
+// Convert the user input to an integer
+var userYear = parseInt(userInput, 10);
+
+// Check if the input is a valid year
+if (isNaN(userYear) || userYear < 1 || userYear > 9999) {
+  alert("Please enter a valid year between 1 and 9999.");
+} else {
+  // Call the function and display the result
+  alert(  daysOfAYear(userYear));
+}
